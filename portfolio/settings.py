52f98aa674bd53be8de37d6037d9a86e26d9d03f
Snipher Marube <sniphermarube@gmail.com>
2025-10-08 12:24:35 +0300

    dockerization and containeraation

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '172.18.0.2',  # Use the service name from docker-compose
+        'PORT': 5432,
     }
 }
 
